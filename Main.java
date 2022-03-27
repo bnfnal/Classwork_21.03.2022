27eaea345a8906c4ca7b3b1b015d483ec802ddf9
Nal Bilalova <59770118+bnfnal@users.noreply.github.com>
2022-03-27 19:21:43 +0300

    Update Main.java
    Добавила реализацию методов

--- Main.java
+++ Main.java
@@ -1,50 +1,80 @@
+package com.company;
+
 import java.util.Scanner;
 
-// Task 1: If you can't sleep, just count sheep!! 
+public class Main {
 
-public class CountingSheeps 
-{
-  public static String countingSheep(int n) {
-    String res = "";
-    for (int i = 1; i<n+1; i++){
-            res += i + " sheep...";
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Task 1: If you can't sleep, just count sheep!!
+	    
+        System.out.println("Task 1: If you can't sleep, just count sheep!!");
+        System.out.println("Введите количество овечек (положительное целое число)");
+        int sheepCount = sc.nextInt();
+        System.out.println("Если не можете уснуть произнесите следующий текст:");
+        System.out.println(CountingSheeps.countingSheep(sheepCount));
+        System.out.println();
+
+        // Task 2: Jaden Casing Strings
+	    
+        System.out.println("Task 2: Jaden Casing Strings");
+        System.out.println("Введите строку");
+        Scanner s = new Scanner(System.in);
+        String l = s.nextLine();
+        System.out.println("Строка в стиле Джейдена Смита:");
+        System.out.println(JadenCase.toJadenCase(l));
+        System.out.println();
+
+        // Task 3: You're a square!
+	    
+        System.out.println("// Task 3: You're a square!");
+        System.out.println("Введите целое число");
+        int n = sc.nextInt();
+        System.out.println("Это число является точным квадратом?");
+        System.out.println(Square.isSquare(n));
+        System.out.println();
+    }
+
+
+// Task 1: If you can't sleep, just count sheep!!
+
+    public class CountingSheeps {
+        public static String countingSheep(int n) {
+            String res = "";
+            for (int i = 1; i < n + 1; i++) {
+                res += i + " sheep...";
+            }
+            return res;
         }
-    return res;
-  }
-}
+    }
 
 // Task 2: Jaden Casing Strings
 
-public class JadenCase 
-{
-  public String toJadenCase(String phrase) 
-  {
-    if (phrase == "" || phrase == null) return null;
-    String[] words = phrase.split(" ");
-    String res = "";
-		for (int i = 0; i<words.length; i++)
-    {
-      res += words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length());
-      if (i<words.length-1) res += " ";
+    public static class JadenCase {
+        public static String toJadenCase(String phrase) {
+            if (phrase == "" || phrase == null) return null;
+            String[] words = phrase.split(" ");
+            String res = "";
+            for (int i = 0; i < words.length; i++) {
+                res += words[i].substring(0, 1).toUpperCase() + words[i].substring(1, words[i].length());
+                if (i < words.length - 1) res += " ";
+            }
+            return res;
+        }
     }
-    return res;
-  }
-}
 
 // Task 3: You're a square!
 
-import java.util.Arrays;
-
-public class Square 
-{    
-    public static boolean isSquare(int n) 
-    { 
-      if (n < 0) return false;
-      if (n == 0) return true;
-      for (int i = 1; i < (int)Math.sqrt(n) + 1; i++)
-      {
-        if (n == i*i) return true;
-      }
-        return false;
+    public class Square {
+        public static boolean isSquare(int n) {
+            if (n < 0) return false;
+            if (n == 0) return true;
+            for (int i = 1; i < (int) Math.sqrt(n) + 1; i++) {
+                if (n == i * i) return true;
+            }
+            return false;
+        }
     }
 }
